7acf318ac115652c33d5354fa76cbe40971045ba
Kristina Spurgin <kspurgin@email.unc.edu>
2019-08-02 11:52:50 -0400

    new query

---
+++ kms_sql_dump/300s_without_sf_codes.sql
@@ -0,0 +1,26 @@
+select 
+'b' || rm.record_num || 'a' AS bnum,
+b.bcode2 AS iiimattype,
+v.field_content
+from sierra_view.varfield v
+inner join sierra_view.bib_record b
+on b.id = v.record_id
+and b.is_suppressed = 'f'
+inner join sierra_view.record_metadata rm
+on rm.id = v.record_id
+inner join sierra_view.bib_record_property bp
+ON b.id = bp.bib_record_id
+where 
+v.marc_tag = '300'
+AND
+((v.field_content ~ ' +: *[^|][^b]'
+AND v.field_content !~ '\|e.*: *[^|][^b]')
+OR
+(v.field_content ~ ' +; *[^|][^c]'
+AND v.field_content !~ '\|e.*; *[^|][^c]')
+OR
+(v.field_content LIKE '% +%'
+AND v.field_content NOT LIKE '%|e%'
+AND v.field_content !~ '\|a[^|]+ \+ \d+ parts?')
+)
+ 
